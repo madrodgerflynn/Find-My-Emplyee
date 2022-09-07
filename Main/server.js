3b0159ebe77ce124f522aafc5010f36bfefdd371
madrodgerflynn <madrodgerflynn@gmail.com>
2022-09-07 17:45:39 -0600

    in wrong dir

--- Main/server.js
+++ Main/server.js
@@ -1,6 +1,11 @@
 const mysql = require("mysql2");
+const express = require("express");
+
+const PORT = processes.env.PORT || 3001;
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "test",
+  password: "root1234",
 });
